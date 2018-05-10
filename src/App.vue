23133aef14a45924d280937267b071290fa334ec
Sheps <thesheps@gmail.com>
2018-05-11 00:09:07 +0100

    wip

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <Editor />
+    <Preview />
+  </div>
+</template>
+
+<script>
+import Editor from "./components/Editor.vue";
+import Preview from "./components/Preview.vue";
+
+export default {
+  components: { Editor, Preview },
+  name: "App"
+};
+</script>
