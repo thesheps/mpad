a2274e7297d28d4288144ead8cad4edebe5ab519
Sheps <thesheps@gmail.com>
2018-05-11 22:36:04 +0100

    BOLD!

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,5 +1,16 @@
 <template>
-    <b-input class="is-full-height" v-model="text" type="textarea"></b-input>
+    <div>
+        <p class="field has-addons">
+            <a class="button" @click="bold"><span class="icon is-small"><i class="fa fa-bold"></i></span></a>
+            <a class="button" @click="italic"><span class="icon is-small"><i class="fa fa-italic"></i></span></a>
+            <a class="button" @click="underline"><span class="icon is-small"><i class="fa fa-underline"></i></span></a>
+            <a class="button" @click="left"><span class="icon is-small"><i class="fa fa-align-left"></i></span></a>
+            <a class="button" @click="center"><span class="icon is-small"><i class="fa fa-align-center"></i></span></a>
+            <a class="button" @click="right"><span class="icon is-small"><i class="fa fa-align-right"></i></span></a>
+        </p>
+
+        <b-input ref="editor" v-model="text" type="textarea"></b-input>
+    </div>
 </template>
 
 <script>
@@ -14,6 +25,43 @@ export default {
         text() {
             this.$emit("input", this.text);
         }
+    },
+
+    methods: {
+        bold() {
+            let start = this.$refs.editor.$refs.textarea.selectionStart;
+            let end = this.$refs.editor.$refs.textarea.selectionEnd;
+            let text = this.text;
+            
+            this.text = this.text.substring(0, start) + "__" + this.text.substring(start, end) + "__" + text.substring(end, text.length)
+        },
+
+        italic() {
+        },
+
+        underline() {
+        },
+
+        left() {
+        },
+
+        center() {
+        },
+
+        right() {
+        }
     }
 };
 </script>
+
+<style scoped>
+.is-full-height {
+    max-height: 100% !important;
+    min-height: 100% !important;
+}
+
+.textarea {
+    height: 100vh !important;
+    max-height: 100vh !important;
+}
+</style>
